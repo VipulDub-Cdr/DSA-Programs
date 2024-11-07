a1d10b8b96d5c0a0e8cf68dc5c75e285de2d583e
Vipul Dubey <vipuldubey041@gmail.com>
2024-11-07 19:47:52 +0530

    largestElement

---
+++ Arrays/Easy/LargestElement.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int largestElement(vector<int> arr){
+    int n = arr.size();
+    int largest = INT_MIN;
+    for(auto it:arr){
+        largest = max(largest,it);
+    }
+    return largest;
+}
+int main(){
+    vector<int> arr = {1,2,32,5,10};
+    cout<<largestElement(arr)<<endl;
+    return 0;
+}
