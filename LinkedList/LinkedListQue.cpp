878e3e716ac882307f437abdf27c523eb7af32ba
Vipul Dubey <vipuldubey041@gmail.com>
2024-12-11 21:20:12 +0530

    cycle

--- LinkedList/LinkedListQue.cpp
+++ LinkedList/LinkedListQue.cpp
@@ -74,15 +74,50 @@ Node* reverseLL3(Node* head)
     return newHead;
 }
 
-void detectCycle(Node* head){
+bool detectCycle(Node* head){
     Node* fast = head;
     Node* slow = head;
-    while(fast!=NULL and fast!=slow){
+    while(fast!=NULL and fast->next!=NULL){
         fast = fast->next->next;
         slow = slow->next;
+        if(slow==fast)
+            return true;
     }
-    
+    return false;
 }
+
+Node* startingPoint1(Node* head){
+    Node* ptr = head;
+    unordered_map<Node*,int> umpp;
+    while(ptr!=NULL and ptr->next!=NULL){
+        if(umpp[ptr])
+            return ptr;
+        umpp[ptr] = 1;
+        ptr = ptr->next;
+    }
+    return NULL;
+}
+
+//Optimized the solution by using tortoise and hare method
+Node* startingPoint2(Node* head){
+    Node* fast = head;
+    Node* slow = head;
+    while(fast!=NULL and fast->next!=NULL){
+        fast = fast->next->next;
+        slow = slow->next;
+        if(fast==slow)
+            break;
+    }
+    slow = head;
+    if(fast!=NULL){
+        while(fast!=slow){
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+    return fast;
+}
+
 void printLL(Node* head){
     Node *temp = head;
     while(temp!=NULL){
@@ -91,20 +126,33 @@ void printLL(Node* head){
     }
 }
 int main() {
-    Node *head = new Node(10);
-    head = insertHead(head,20);
-    head = insertHead(head,30);
-    head = insertHead(head,40);
-    head = insertHead(head,50);
-    printLL(head);
-    cout<<endl;
+    // Node *head = new Node(10);
+    // head = insertHead(head,20);
+    // head = insertHead(head,30);
+    // head = insertHead(head,40);
+    // head = insertHead(head,50);
+    // printLL(head);
+    // cout<<endl;
     
     // Node* midNode = middleNode(head);
     // cout<<midNode->data<<endl;
 
     // head = reverseLL1(head);
     // head = reverseLL2(head);
-    head = reverseLL3(head);
-    printLL(head);
+    // head = reverseLL3(head);
+    // cout<<detectCycle(head);
+    // printLL(head);
+
+    //cycled LL with starting point Second Node with value 20
+    Node* head = new Node(10,NULL);
+    Node* second = new Node(20,NULL);
+    head->next = second;
+    Node* third = new Node(30,NULL);
+    second->next = third;
+    Node* fourth = new Node(40,NULL);
+    third->next = fourth;
+    fourth->next = second;
+
+    cout<<startingPoint2(head)->data<<endl;
     return 0;
 }
